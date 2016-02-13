6e8b525a45e186a3521d67da67d892c504b0d532
ismayc <chester.ismay@gmail.com>
2016-02-13 12:29:48 -0800

    Added documentation using roxygen for the reedtemplates packages, and functions (reed_thesis, label, and ref)

--- R/reed_thesis.R
+++ R/reed_thesis.R
@@ -1,4 +1,19 @@
+#' Creates an R Markdown PDF Thesis document
+#'
+#' This is a function called in output in the YAML of the driver Rmd file to specify using the Reed
+#' Senior Thesis LaTeX template and cls files.
+#'
 #' @export
+#' @param toc A Boolean (TRUE or FALSE) specifying where table of contents should be created
+#' @param toc_depth A positive integer
+#' @return A modified \code{pdf_document} based on the Reed Senior Thesis LaTeX
+#'   template
+#' @examples
+#' \dontrun{
+#'  output:
+#'    reedtemplates::reed_thesis:
+#'      toc: true
+#' }
 reed_thesis <- function(toc = TRUE, toc_depth = 3) {
   template <- find_resource("reed_thesis", "template.tex")
 
@@ -42,7 +57,36 @@ reed_thesis <- function(toc = TRUE, toc_depth = 3) {
   base
 }
 
+#' Adds a label to a figure/chemical reaction and includes the figure in the
+#' document
+#'
+#' This allows users to label and include R and non-R generated objects into
+#' their document
+#'
 #' @export
+#' @param path A string specifying where the object is stored as a file
+#' @param caption A string providing the text that will appear with the object
+#' @param label A string providing a name to use with \code{ref}
+#' @param type A string (either "figure" or "chem.reac") providing object type
+#' @param alt.cap A string giving an alternative caption to be included in the
+#'   Table of Figures
+#' @param cap.size A string giving the size of the caption.  Options are
+#'   "scriptsize", "footnotesize", "small", "normalsize", "large", and "Large"
+#' @param scale A numeric specifying the scale to display a figure (1
+#'   corresponds to 100\% of original size, 0.5 corresponds to 50\% of original
+#'   size, etc.)
+#' @param angle A numeric specifying the degree to rotate a figure
+#' @param options A string attempting to fix some of the "floating" issues with
+#'   LaTeX images
+#' @return LaTeX figure environment code that can be run with R chunk option
+#'   \code{results="asis"} to produce object in document with given label and
+#'   features
+#' @examples
+#' \dontrun{
+#'  output:
+#'    reedtemplates::reed_thesis:
+#'      toc: true
+#' }
 label <- function(path = NULL,
                   caption = "Default caption",
                   label = "def",
@@ -51,7 +95,7 @@ label <- function(path = NULL,
                   cap.size = "normalsize",
                   scale = 1,
                   angle = 0,
-                  options = "htbp"){
+                  options = "h!tbp"){
   if(type == "figure"){
     cat(
       paste0(
@@ -83,7 +127,21 @@ label <- function(path = NULL,
 
 }
 
+#' References previously created label
+#'
+#' This allows users to make a reference to an object imported using the
+#' \code{label} function
+#'
 #' @export
+#' @param label A string specifying object label given in \code{label} function
+#'   call
+#' @param type A string corresponding to "table" or "figure" label
+#' @return LaTeX code to provide object number corresponding to \code{label}
+#'   specified in \code{label} function
+#' @examples
+#' \dontrun{
+#'  `r ref("subd2", type = "figure")`
+#' }
 ref <- function(label = "def", type = "figure"){
   paste0("\\autoref{",
          ifelse(type == "figure", "fig:",
---
+++ R/reedtemplates.R
@@ -0,0 +1,14 @@
+#' reedtemplates: A package for creating documents using R Markdown
+#'
+#' The reedtemplates package provides three important functions:
+#' reed_thesis, label, and ref.
+#'
+#' @section reed_thesis:
+#' Creates an R Markdown senior thesis template
+#' @section label:
+#' Adds a label to a figure/chemical reaction and includes the figure in the document
+#' @section ref:
+#' Allows references to be made to labelled objects inline in the document
+#' @docType package
+#' @name reedtemplates
+NULL
